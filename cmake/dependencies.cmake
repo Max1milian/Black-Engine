256079d4546eea13809bdde5aba8fc9e554d80a1
Maximilian Stocker <max.stocker@gmx.de>
2022-07-30 11:51:01 +0200

    added CMakeLists.txt in every src subfolder

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,8 @@
+find_package(Vulkan REQUIRED)
+find_package(glfw3 CONFIG REQUIRED)
+find_package(glm CONFIG REQUIRED)
+find_package(OpenAL CONFIG REQUIRED)
+find_package(asio CONFIG REQUIRED)
+find_package(fmt CONFIG REQUIRED)
+find_package(glad CONFIG REQUIRED)
+find_package(EASTL CONFIG REQUIRED)
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fmt/core.h>
+#include "vkfw.hpp"
+int main(void)
+{
+	fmt::print("Testprint\n");
+	return 0;
+}
